67f1c22f2a7f77917316e54393dff986df753c92
s.knoll@bkr.de <s.knoll@bkr.de>
2025-12-21 11:37:54 +0100

    account: remove my plants

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -6,17 +6,28 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  styleUrls: ['./account.component.scss'],
 })
+
 export class AccountComponent {
   myPlants$: Observable<IPlant[]>;
 
-  constructor(private accountservice: AccountService) {
-    this.myPlants$ = this.accountservice.myPlants$;
+  constructor(private accountService: AccountService) {
+    this.myPlants$ = this.accountService.myPlants$;
   }
 
   onImgError(event: Event): void {
-  const element = event.target as HTMLImageElement;
-  element.src = 'assets/img/default.jpg';
-}
+    const element = event.target as HTMLImageElement;
+    element.src = 'assets/img/default.jpg';
+  }
+
+  removeFromAccount(plantId: number): void {
+    this.accountService.remove(plantId);
+    console.log('Pflanze (Id)', plantId, 'wurde aus dem Account entfernt');
+  }
+
+   addTask(plantId: number): void {
+    //TODO: Implement addToDo functionality
+    console.log('Aufgabe für Pflanze (Id)', plantId, 'wurde erstellt');
+  }
 }
